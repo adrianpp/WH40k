19b2141af116b7488fbccea3c8d1385c5a28dc2b
Adrian Park <apark@wrs.com>
2017-05-31 05:33:16 -0700

    Playing around with some number crunch for shooting phase

---
+++ math.cc
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <memory>
+#include <map>
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+class NumberSource {
+public:
+    virtual int getValue()=0;
+};
+
+class Constant : public NumberSource {
+public:
+    Constant(int v) : _v(v) {}
+    virtual int getValue(){return _v;}
+private:
+    int _v;
+};
+
+class Dice : public NumberSource {
+public:
+    Dice(int n, int s) : _n(n), _s(s) {}
+    int getNum() {return _n;}
+    int getSize() {return _s;}
+    int roll()
+    {
+        int total = 0;
+        if(DEBUG) std::cerr << "{";
+        for(int i = 0; i < getNum(); ++i)
+        {
+            if(DEBUG) if( i != 0) std::cerr << ",";
+            int roll = 1 + std::rand() % getSize();
+            if(DEBUG) std::cerr << roll;
+            total += roll;
+        }
+        if(DEBUG) std::cerr << "}";
+        return total;
+    }
+    int rollGreaterEqual(int value)
+    {
+        int total = 0;
+        if(DEBUG) std::cerr << "{";
+        for(int i = 0; i < getNum(); ++i)
+        {
+            if(DEBUG) if( i != 0) std::cerr << ",";
+            int roll = 1 + std::rand() % getSize();
+            if(DEBUG) std::cerr << roll;
+            if( roll >= value )
+                ++total;
+        }
+        if(DEBUG) std::cerr << "}";
+        return total;
+    }
+    virtual int getValue(){return roll();}
+private:
+    int _n, _s;
+};
+
+class Attribute {
+public:
+    Attribute(std::string name) : _name(name) {}
+    std::string getName(){return _name;}
+    bool operator == (Attribute rhs){return rhs.getName() == getName();}
+public:
+    std::string _name;
+};
+
+class Weapon {
+public:
+    Weapon(std::string name, int s, std::shared_ptr<NumberSource> ap, std::shared_ptr<NumberSource> n, std::shared_ptr<NumberSource> d) : _name(name), _s(s), _ap(ap), _n(n), _d(d) {}
+    std::string getName(){return _name;}
+    int getStrength(){return _s;}
+	int getArmourPiercingValue(){return _ap->getValue();}
+    int getNumShots(){return _n->getValue();}
+    int getDamage(){return _d->getValue();}
+    Weapon& attr(Attribute a){attrs.push_back(a); return *this;}
+    bool hasAttr(Attribute a){
+        for(auto AI = attrs.begin(); AI != attrs.end(); ++AI)
+            if( a == *AI ) return true;
+        return false;
+    }
+    std::vector<Attribute>& attr(){return attrs;}
+    int& property(std::string s) {return propMap[s];}
+private:
+    std::string _name;
+    int _s;
+    std::shared_ptr<NumberSource> _ap, _n, _d;
+    std::vector<Attribute> attrs;
+    std::map<std::string, int> propMap;
+};
+
+class Model {
+public:
+    Model(std::string name, int m, int ws, int bs, int s, int t, int w, int a, int ld, int as, std::vector<Weapon> weapons) : _name(name), _m(m), _ws(ws), _bs(bs), _s(s), _t(t), _w(w), _a(a), _ld(ld), _as(as), _weapons(weapons) {}
+    std::string getName() {return _name;}
+    virtual int getMovement(){return _m;}
+    virtual int getWeaponSkill(){return _ws;}
+    virtual int getBallisticSkill(){return _bs;}
+    virtual int getStrength(){return _s;}
+    int getToughness(){return _t;}
+    int getWounds(){return _w;}
+    virtual int getAttacks(){return _a;}
+    int getLeadership(){return _ld;}
+    int getArmourSave(){return _as;}
+    std::vector<Weapon>& getWeapons(){return _weapons;}
+private:
+   std::string _name;
+   int _m, _ws, _bs, _s, _t, _w, _a, _ld, _as;
+   std::vector<Weapon> _weapons;
+};
+
+int getWoundsSuffered(Model shooter, Weapon w, Model shootee)
+{
+    //roll to hit - number of shots of weapon, rolling over shooter ballistic skill
+    if(DEBUG) std::cerr << "[numShots=";
+    int numShots = w.getNumShots();
+    if(DEBUG) std::cerr << numShots << ",numHits=";
+    int numHits = Dice(numShots, 6).rollGreaterEqual(shooter.getBallisticSkill());
+    if( w.hasAttr(Attribute("autohit")) ) numHits = numShots;
+    if(DEBUG) std::cerr << numHits;
+    w.property("num_hits") = numHits;
+    //roll to wound
+    int woundOn = -1;
+    if( w.getStrength() * 2 <= shootee.getToughness() ) 
+        woundOn = 6; //wound on 5+
+    else if(w.getStrength() < shootee.getToughness() )
+        woundOn = 5;
+    else if(w.getStrength() == shootee.getToughness() )
+        woundOn = 4;
+    else if(w.getStrength() >= 2 * shootee.getToughness() )
+        woundOn = 2;
+    else if(w.getStrength() > shootee.getToughness() )
+        woundOn = 3;
+    if(DEBUG) std::cerr << ",woundOn=" << woundOn << "+,numWounds=";
+    int numWounds = Dice(numHits,6).rollGreaterEqual(woundOn);
+    if(DEBUG) std::cerr << numWounds;
+    //roll saves
+    if(DEBUG) std::cerr << ",saveOn=" << shootee.getArmourSave() - w.getArmourPiercingValue() << "+,numSaved=";
+    int numSaved = Dice(numWounds,6).rollGreaterEqual(shootee.getArmourSave() - w.getArmourPiercingValue());
+    if(DEBUG) std::cerr << numSaved;
+    //calculate wounds suffered
+    if(DEBUG) std::cerr << ",woundsSuffered=";
+    int woundsSuffered = 0;
+    for(int c = 0; c < numWounds - numSaved; ++c)
+    {
+        int roll = w.getDamage();
+        //a model can't suffer more wounds than it has
+        if( roll > shootee.getWounds() ) roll = shootee.getWounds();
+        woundsSuffered += roll;
+    }
+    if(DEBUG) std::cerr << woundsSuffered << "]";
+    return woundsSuffered;
+}
+
+template<int i>
+class C : public std::shared_ptr<NumberSource> {
+public:
+    C() : std::shared_ptr<NumberSource>(new Constant(i)) {}
+};
+
+template<int N, int S>
+class D : public std::shared_ptr<NumberSource> {
+public:
+    D() : std::shared_ptr<NumberSource>(new Dice(N,S)) {}
+};
+
+template<class F>
+class FunctionalNumberSource : public NumberSource {
+public:
+    FunctionalNumberSource(F f) : _f(f) {}
+    virtual int getValue() {return _f();}
+private:
+    F _f;
+};
+
+template<class F>
+std::shared_ptr<NumberSource> functionalSource(F f)
+{
+    return std::shared_ptr<NumberSource>(new FunctionalNumberSource<F>(f));
+}
+
+int main()
+{
+    std::srand(time(NULL));
+    std::shared_ptr<NumberSource> C0{C<0>()}, C1{C<1>()}, D3{D<1,3>()}, D6{D<1,6>()};
+    Weapon bolter("bolter", 4, C0, C1, C1);
+    Weapon lascannon("lascannon", 9, C<-3>(), C1, D6);
+    Weapon acid_spray("acidSpray", 7/*actually user*/, C<-1>(), D6, D3);
+    acid_spray.attr(Attribute("autohit"));
+    Weapon fleshborer_hive("fleshborerHive", 5, C0, C<20>(), C1);
+    Weapon stinger_salvo("stingerSalvo", 5, C<-1>(), C<4>(), C1);
+    Weapon rupture_cannon("ruptureCannon", 10, functionalSource([&rupture_cannon](){if(rupture_cannon.property("num_hits") == 2) return -4; return -1;}), C<2>(), functionalSource([&rupture_cannon](){if(rupture_cannon.property("num_hits") == 2) return Dice(1,6).roll(); return 2;}));
+    Weapon fleshborer("fleshborer", 4, C0, C1, C1);
+    Weapon devourer("devourer", 4, C0, C<3>(), C1);
+    std::vector<Model> allModels = {
+            Model("SpaceMarine", 6, 3, 3, 4, 4, 1, 1, 8, 3, {bolter, lascannon}),
+            Model("Tyrannofex", 6, 4, 4, 7, 8, 14, 4, 7, 3, {acid_spray, fleshborer_hive, stinger_salvo, rupture_cannon}),
+            Model("Termagant", 6, 4, 4, 3, 3, 1, 1, 5, 6, {fleshborer, devourer}),
+            Model("TyranidWarrior", 6, 3, 4, 4, 4, 3, 3, 9, 4, {devourer}),
+            Model("Carnifex", 7, 4, 4, 6, 7, 8, 4, 6, 3, {})
+        };
+
+    for(int m1 = 0; m1 < allModels.size(); ++m1)
+        for(auto W = allModels[m1].getWeapons().begin(); W != allModels[m1].getWeapons().end(); ++W)
+            for(int m2 = 0; m2 < allModels.size(); ++m2)
+            {
+                int iters = 10000;
+                int woundsSuffered = 0;
+                for(int i = 0; i < iters; ++i)
+                    woundsSuffered += getWoundsSuffered(allModels[m1], *W, allModels[m2]);
+                std::cout << allModels[m1].getName() << "." << W->getName() << " -> " << allModels[m2].getName() << " : " << float(woundsSuffered)/iters << std::endl;
+            }
+
+    return 0;
+}
